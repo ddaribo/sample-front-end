f2b2264b574f143d1a6472434f8c1d400b6f31be
ddaribo <bozhidara.p@abv.bg>
2020-12-10 21:36:30 +0200

    Changed font and added material cards to display animal list

--- src/app/animals/animals.module.ts
+++ src/app/animals/animals.module.ts
@@ -1,20 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AnimalsComponent } from './animals/animals.component';
-import { AnimalService } from '../animal.service';
-import { AnimalDetailsComponent } from './animal-details/animal-details.component';
-import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AnimalsComponent } from "./animals/animals.component";
+import { AnimalService } from "../animal.service";
+import { AnimalDetailsComponent } from "./animal-details/animal-details.component";
+import { RouterModule } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatGridListModule } from "@angular/material/grid-list";
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule
   ],
   declarations: [AnimalsComponent, AnimalDetailsComponent],
   exports: [AnimalsComponent],
   providers: [AnimalService]
 })
-export class AnimalsModule { }
+export class AnimalsModule {}
