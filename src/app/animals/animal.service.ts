95718de35cb91bc6f05a5d8c3c35482ced7ecad1
alexa984 <a.yovkova@hacksoft.io>
2021-01-12 01:57:42 +0200

    Fix JWT authorization

--- src/app/animals/animal.service.ts
+++ src/app/animals/animal.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from "@angular/core";
 import { Observable, BehaviorSubject } from "rxjs";
-import { delay, shareReplay } from "rxjs/operators";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { tap } from "rxjs/operators";
 import { Post } from "../shared/models/post";
-import { backendURL, postsURL, postsCreateURL, fakeAnimalsApiUrl } from "src/utils";
+import { backendURL, postsCreateURL, fakeAnimalsApiUrl } from "src/utils";
 
 @Injectable()
 export class AnimalService {
-  private subject = new BehaviorSubject<Post[]>(null);
+  private subject = new BehaviorSubject<Post>(null);
   constructor(private http: HttpClient) {}
 
   getData() {
@@ -17,6 +16,32 @@ export class AnimalService {
 
   public getPosts(): Observable<Post[]> {
     return this.http.get<Post[]>(fakeAnimalsApiUrl);
+  }
+
+  public createPost(postFormData): Observable<Post> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        'Authorization': `JWT ${JSON.parse(localStorage.getItem('Authorization'))}`,
+
+      }),
+    }
+    return this.http
+      .post<Post>(backendURL + postsCreateURL, postFormData, httpOptions)
+      .pipe(
+        tap((post) => {
+          this.subject.next(post);
+        })
+      );
+  }
+  /*
+  loadAllAnimals(): Observable<Animal[]> {
+    return this.http.get<Animal[]>("/api/animals")
+      .pipe(
+          map(res => res["animals"]), // or sth else
+          // only trigger one http request for this observable; otherwise every subscription issues a separate http request
+          shareReplay()
+      );
   }
 
   public createPost(postFormData) {
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -12,7 +12,8 @@ import { backendURL, camelToSnakeCase, loginURL, registerURL } from "src/utils";
 import { User } from "../shared/models/user";
 
 // Local storage key under which the user profile is saved
-const AUTH_DATA = "auth_data";
+const AUTH_DATA = "Authorization";
+const USER_DATA = "User";
 
 @Injectable({
   providedIn: "root",
@@ -29,7 +30,7 @@ export class AuthService {
 
     this.isLoggedOut$ = this.isLoggedIn$.pipe(map((loggedIn) => !loggedIn));
 
-    const user = localStorage.getItem(AUTH_DATA); //string
+    const user = localStorage.getItem(USER_DATA); //string
 
     if (user) {
       this.subject.next(JSON.parse(user));
@@ -42,7 +43,8 @@ export class AuthService {
       .pipe(
         tap((user) => {
           this.subject.next(user);
-          localStorage.setItem(AUTH_DATA, JSON.stringify(user));
+          localStorage.setItem(AUTH_DATA, JSON.stringify(user['token']));
+          localStorage.setItem(USER_DATA, JSON.stringify(user));
         }),
         // Avoid multiple calls to this api endpoint
         shareReplay()
@@ -53,6 +55,7 @@ export class AuthService {
     this.subject.next(null);
     // TODO: Should call the logout API
     localStorage.removeItem(AUTH_DATA);
+    localStorage.removeItem(USER_DATA);
   }
 
   public register(userData: any): Observable<User> {
